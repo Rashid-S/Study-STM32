40550b2210cbf059ff8255354b9e9f0159bc479f
Rashid-S <rashid_s@inbox.ru>
2020-11-17 15:48:14 +0300

    очередные корректировки

--- Lesson_1/user/main.c
+++ Lesson_1/user/main.c
@@ -4,7 +4,11 @@
 
 int main(void)
 {
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; //
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // vkluchim taktirovanie porta B
+	GPIOB->MODER = 0x40000000;            // vkluchim 15 nozku porta B
+	GPIOB->OTYPER = 0;
+	GPIOB->OSPEEDR = 0;
+	GPIOB->ODR = 0x8000;	
 	while(1)
 	{
 	}
